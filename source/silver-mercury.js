8dbbbc073ed00943a512c8208281c1487523d0b1
Unknown <b6010572@my.shu.ac.uk>
2017-12-26 19:15:30 +0000

    Double buffering problem

--- source/silver-mercury.js
+++ source/silver-mercury.js
@@ -3,6 +3,8 @@ document.body.onload = function () {
   game.start();
 }
 
+var x = 0;
+
 var game = {
   node: document.getElementById("silver-mercury"),
   canvas: {
@@ -11,12 +13,15 @@ var game = {
     height: 640,
     get context () {
       return this.node.getContext("2d");
+    },
+    clear: function () {
+      this.context.clearRect(0, 0, this.width, this.height);
     }
   },
   start: function () {
     this.node.parentNode.appendChild(this.canvas.node);
-    game.canvas.node.width = this.canvas.width;
-    game.canvas.node.height = this.canvas.height;
+    game.canvas.node.width = game.canvas.width;
+    game.canvas.node.height = game.canvas.height;
     game.canvas.node.style.border = "1px solid gray";
 
     window.requestAnimationFrame(game.update);
@@ -28,7 +33,9 @@ var game = {
     window.requestAnimationFrame(game.update);
   },
   draw: function () {
-    game.canvas.context.moveTo(0,0);
+    game.canvas.clear();
+
+    game.canvas.context.moveTo(x, 0);
     game.canvas.context.lineTo(200, 100);
     game.canvas.context.stroke();
 
