6c586d4ab6b392fe98114f74548162afe789df17
Unknown <b6010572@my.shu.ac.uk>
2017-12-26 18:50:46 +0000

    Resolved scoping issue

--- source/silver-mercury.js
+++ source/silver-mercury.js
@@ -9,20 +9,29 @@ var game = {
     node: document.createElement("canvas"),
     width: 360,
     height: 640,
-    context: this.node.getContext("2d")
+    get context () {
+      return this.node.getContext("2d");
+    }
   },
-  start: function() {
-    // Setup canvas
+  start: function () {
     this.node.parentNode.appendChild(this.canvas.node);
+    game.canvas.node.width = this.canvas.width;
+    game.canvas.node.height = this.canvas.height;
+    game.canvas.node.style.border = "1px solid gray";
 
-    this.canvas.node.width = this.canvas.width;
-    this.canvas.node.height = this.canvas.height;
-
-    this.canvas.node.style.border = "1px solid gray";
-
-    this.update();
+    window.requestAnimationFrame(game.update);
+    window.requestAnimationFrame(game.draw);
   },
-  update: function() {
-    // TODO: make core game loop
+  update: function () {
+    x++;
+
+    window.requestAnimationFrame(game.update);
+  },
+  draw: function () {
+    game.canvas.context.moveTo(0,0);
+    game.canvas.context.lineTo(200, 100);
+    game.canvas.context.stroke();
+
+    window.requestAnimationFrame(game.draw);
   }
 };
